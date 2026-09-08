4bb1184813b120b6eb30392294395d2355620f13
NMO-SOC <nicholas.morlin@education.vic.gov.au>
2026-09-09 09:38:34 +1000

    Subscribe to all team, hint, and event changes

--- assets/hunt.js
+++ assets/hunt.js
@@ -170,7 +170,7 @@ async function askHint() {
 function fail(t) { stageEl.innerHTML = `<div class="notice bad">${t}</div>`; }
 
 // Live: hints arriving, locks, teacher moving your stage
-sb.channel('me')
+sb.channel('realtime:public')
   .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'teams' }, load)\n  .on('postgres_changes', { event: '*', schema: 'public', table: 'hints' }, load)
   .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'teams' }, load)\n  .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'teams' }, load)
   .subscribe();
